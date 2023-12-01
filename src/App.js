59f3dff88350909756c4c650b339a6f38d25535f
keysie23 <147827496+keysie23@users.noreply.github.com>
2023-12-01 14:10:50 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { styles } from './styles';
+import image from './image/computer.jpg';
 
-function App() {
+const LoginBox = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.boxStyle}>
+      <h2 // Inline Styling
+        style={{ textAlign: 'center', color: 'white', fontSize: '30px' }}>Login</h2> 
+
+      <input
+        placeholder="Username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          style={{ ...styles.inputStyle, color: 'black' }}
+        />
+
+      <input
+        placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          style={{ ...styles.inputStyle, color: 'black' }}
+        />
+  
+
+      <a href='' style={{ textAlign: 'center', display: 'black', marginTop: '30px', marginLeft: '170px',color: 'white' }}>Forgot Password</a>
+
+      <button onClick={handleLogin} style={{ ...styles.buttonStyle, color: 'white' }}>
+        Login
+      </button>
+      <p style={{color: 'white', marginLeft: '120px'}}>Don't have an account?<a href='' style={{ textAlign: 'center', display: 'black', marginTop: '30px', marginLeft: '10px',color: 'white' }}>Signup</a></p>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div style={bodyStyle}>
+      <div style={boxStyle}>
+        <LoginBox />
+      </div>
+    </div>
+  );
+};
+
+const bodyStyle = { // Style Object Variable
+  backgroundImage: `url(${image})`,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100vh',
+};
+
+const boxStyle = {
+  margin: 'auto',
+  width: '500px',
+  height: '500px',
+  padding: '20px',
+  border: '5px solid #ccc',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+  marginTop: '80px',
+  boxSizing: 'border-box',
+  borderRadius: '50px',
+};
 
 export default App;
+
---
+++ src/styles.js
@@ -0,0 +1,8 @@
+
+  const inputStyle = {width: '85%', padding: '10px', margin: '8px 0', marginRight: '27px', marginLeft: '30px', boxSizing: 'border-box', borderRadius: '10px'}
+  const buttonStyle = {width: '370px', padding: '10px', margin: '20px', marginLeft: '35px', backgroundColor: '#4caf50', border: 'none', borderRadius: '8px', cursor: 'pointer'}
+  
+  export const styles = {
+    inputStyle: inputStyle,
+    buttonStyle: buttonStyle, // sharing style across components
+  };
